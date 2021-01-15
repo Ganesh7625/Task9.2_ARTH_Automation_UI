304d537e6e555f9588bedd5c6992ac8d949baf30
Laveena Jethani <laveena.j.21@gmail.com>
2021-01-15 17:53:30 +0530

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+print("content-type: text/plain")
+print()
+import subprocess as sp
+import cgi
+#form =   cgi.FieldStorage()
+cmd1 = "sudo yum install httpd -y"
+output1 = sp.getstatusoutput(cmd1)
+status1 = output1[0]
+out1 = output1[1]
+cmd2 = "sudo systemctl start httpd"
+output2 = sp.getstatusoutput(cmd2)
+status2 = output2[0]
+out2 = output2[1]
+cmd3 = "sudo systemctl enable httpd"
+output3 = sp.getstatusoutput(cmd3)
+status3 = output3[0]
+out3 = output3[1]
+
+
+if status1 == 0 :
+        print("Apache software installed successfully")
+        if status == 2:
+            print("Apache webserver service is started")
+            if status == 3:
+                print("Apache webservice is enabled")
+            else:
+                print("error occuring during Apache webservice enable")
+                print(output3)
+        else:
+            print("error occuring during Apache webservice start")
+            print(output2)
+
+else:
+    print("error occuring during Apache webserver installing")
+    print(output1)
+
+
